b8eadbf3a71fdf3fe8fe641bf73fe04152afc889
Reinaldo Portocarrero <juniorportom@gmail.com>
2019-02-03 19:51:16 -0500

    Se agrega codigo de registro exitoso

---
+++ cypress/integration/taller2/registro_exitoso_epec.js
@@ -0,0 +1,16 @@
+describe('Los estudiantes login', function() {
+    it('Visits los estudiantes and fails at login', function() {
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+            //Lineas nuevas  
+        cy.contains('Ingresar').click()
+        cy.get('.cajaSignUp').find('input[name="nombre"]').click().type("Junior")
+        cy.get('.cajaSignUp').find('input[name="apellido"]').click().type("Portocarrero")
+        cy.get('.cajaSignUp').find('input[name="correo"]').click().type("prueba2@example.com")
+        cy.get('.cajaSignUp').find('select[name="idPrograma"]').select("Ingeniería de Sistemas y Computación")
+        cy.get('.cajaSignUp').find('input[name="password"]').click().type("12345678")
+        cy.get('.cajaSignUp').find('input[name="acepta"]').click()
+        cy.get('.cajaSignUp').contains('Registrarse').click()
+        cy.contains('Registro exitoso!')
+    })
+})
