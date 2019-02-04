33063c9f6d3a282971fc0378e2a29f4640d7d478
Reinaldo Portocarrero <juniorportom@gmail.com>
2019-02-03 19:30:26 -0500

    Se agrega codigo inicial de visita al sitio

---
+++ cypress/integration/taller2/simple_spec.js
@@ -0,0 +1,6 @@
+describe('Los estudiantes login', function() {
+    it('Visits los estudiantes and fails at login', function() {
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+    })
+})
